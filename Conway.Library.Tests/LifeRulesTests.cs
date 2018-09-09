3c535d6ad37f5364b0a89480eed2023d02fdd819
Chris Keene <keenecrk@gmail.com>
2018-09-09 14:52:10 -0700

    Implemented rules, all tests pass

--- Conway.Library.Tests/LifeRulesTests.cs
+++ Conway.Library.Tests/LifeRulesTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,68 @@ using System.Threading.Tasks;
 
 namespace Conway.Library.Tests
 {
+    /*****Rules*****
+     * Any live cell with fewer than two live neighbors dies.
+     * Any live cell with two or three live neighbors lives.
+     * Any live cell with more than three live neighbors dies
+     * Any dead cell with exactly three live neighbors becomes a live cell
+     */
+
+    [TestFixture]
     public class LifeRulesTests
     {
+        [Test]
+        public void LiveCell_FewerThan2LiveNeighbors_Dies(
+            [Range(0,1)] int liveNeighbors)
+        {
+            var currentState = CellState.Alive;
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+            Assert.AreEqual(CellState.Dead, newState);
+        }
+
+        [Test]
+        public void LiveCell_2Or3LiveNeighbors_Lives(
+            [Range(2, 3)] int liveNeighbors)
+        {
+            var currentState = CellState.Alive;
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+            Assert.AreEqual(CellState.Alive, newState);
+        }
+
+        [Test]
+        public void LiveCell_MoreThan3LiveNeighbors_Dies(
+            [Range(4, 8)] int liveNeighbors)
+        {
+            var currentState = CellState.Alive;
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+            Assert.AreEqual(CellState.Dead, newState);
+        }
+
+        [Test]
+        public void DeadCell_Exactly3LiveNeighbores_BecomesAlive()
+        {
+            var liveNeighbors = 3;
+            var currentState = CellState.Dead;
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+            Assert.AreEqual(CellState.Alive, newState);
+        }
+
+        [Test]
+        public void DeadCell_LessThan3LiveNeighbors_StaysDead(
+            [Range(0, 2)] int liveNeighbors)
+        {
+            var currentState = CellState.Dead;
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+            Assert.AreEqual(CellState.Dead, newState);
+        }
+
+        [Test]
+        public void DeadCell_MoreThan3LiveNeighbors_StaysDead(
+            [Range(4, 8)] int liveNeighbors)
+        {
+            var currentState = CellState.Dead;
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+            Assert.AreEqual(CellState.Dead, newState);
+        }
     }
 }
--- Conway.Library/LifeRules.cs
+++ Conway.Library/LifeRules.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Conway.Library
 {
+    public enum CellState
+    {
+        Alive,
+        Dead,
+    }
+
     public class LifeRules
     {
+        public static CellState GetNewState(CellState currentState, int liveNeighbors)
+        {
+            if (currentState == CellState.Alive && liveNeighbors < 2)
+                return CellState.Dead;
+            if (currentState == CellState.Alive && liveNeighbors > 3)
+                return CellState.Dead;
+            if (currentState == CellState.Dead && liveNeighbors == 3)
+                return CellState.Alive;
+            return currentState;
+        }
     }
 }
